0327fe31b46d71c18c440a757bc0106b496dc7eb
luchocarlos <luchopulimar@gmail.com>
2026-03-15 20:26:29 -0500

    backend conecte fontende vercel

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -8,7 +8,7 @@ export default [
     config: {
       origin: [
         'http://localhost:5173',
-        process.env.FRONTEND_URL || '',   //ajuste para deploy en Vercel
+        'https://reto-factus-six.vercel.app',   // configuracion para frontend en vercel
       ].filter(Boolean),
       headers: ['Content-Type', 'Authorization'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
